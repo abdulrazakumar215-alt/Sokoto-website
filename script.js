083045ef5f3bd59a0ae488bb8091626b7e27fc88
abdulrazakumar215-alt <abdulrazakumar215@gmail.com>
2025-10-06 23:17:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-alert("Barka da zuwa website na Sokoto!");function gaisuwa() {
-document.getElementById("sako").innerHTML = "Ina maka fatan alheri, Sokoto!";
+alert("Barka da zuwa website na Sokoto!");
+function gaisuwa() {
+  document.getElementById("sako").innerHTML = "Ina maka fatan alheri, Sokoto!";
 }
